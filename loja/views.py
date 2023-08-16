b7a5ba7875cae495ce855ec986df632797b65708
murillojose15 <90987604+MurilloJose15@users.noreply.github.com>
2023-08-15 22:04:52 -0400

    Initial commit

---
+++ loja/views.py
@@ -0,0 +1,26 @@
+from django.views.generic import ListView, DetailView
+
+from loja.models import Categoria, Produto
+
+class ProdutoListView(ListView):
+    template_name = 'loja/produtos/listar.html'
+    model = Produto
+    queryset = Produto.disponiveis.all()
+    context_object_name = 'produtos'
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        categoria = None
+        categorias = Categoria.objects.all()
+        if self.kwargs and self.kwargs['categ_slug']:
+            slug = self.kwargs['categ_slug']
+            categoria = Categoria.filter(slug=slug)
+            self.queryset = self.queryset.filter(categoria=categoria)
+        contexto['categoria'] = categoria
+        contexto['categorias'] = categorias
+        return contexto
+
+
+class ProdutoDetailView(DetailView):
+    template_name = 'loja/produtos/detalhe.html'
+    model = Produto
