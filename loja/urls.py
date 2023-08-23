3c654bf01c0ab15178f44122bd795ed619b16dc8
murillojose15 <muriloj732@gmail.com>
2023-08-22 23:23:44 -0400

    up

--- loja/urls.py
+++ loja/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('produtos', views.ProdutoListView.as_view(), name='listarprod'),
     path('<slug:categ_slug>', views.ProdutoListView.as_view(), name='listarprodcateg'),
-    path('produto/<pk>/<slug>', views.ProdutoListView.as_view(), name='detalheprod'),
+    path('produto/<pk>/<slug>', views.ProdutoDetailView.as_view(), name='detalheprod'),
 ]
